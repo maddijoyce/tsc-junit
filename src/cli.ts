2678fc0835897d60c4ed0c5081bc83f7fc212a8d
Maddi Joyce <maddi.joyce@smokeball.com>
2022-10-01 22:09:27 +0930

    feat: 🎸 add force option
    Add a force option that will output xml even when stdin is empty

--- src/cli.ts
+++ src/cli.ts
@@ -4,7 +4,7 @@ import { createXML } from './junit';
 import { stdinRead } from './utils';
 
 export async function start() {
-  const { output } = await yargs(hideBin(process.argv))
+  const { output, force } = await yargs(hideBin(process.argv))
     .locale('en')
     .option('output', {
       alias: 'o',
@@ -12,11 +12,17 @@ export async function start() {
       description: 'Output file path',
       default: 'tsc-junit.xml',
     })
+    .option('force', {
+      alias: 'f',
+      type: 'boolean',
+      description: 'Run with empty input',
+      default: false,
+    })
     .usage('tsc-junit\nUsage: tsc --noEmit | tsc-junit')
     .parse();
 
   const stdin = await stdinRead();
-  if (stdin) {
+  if (stdin || force) {
     try {
       await createXML(output, stdin);
     } catch (e) {
